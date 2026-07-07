6ab20a3eadf2863dd0d27f8b01f4aa0f93ac0edd
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-07-07 14:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (91.39%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode* temp=head;
+    struct ListNode* pos=head;
+    int count=0;
+    while(temp!=NULL) {
+        temp=temp->next;
+        count++;
+    }
+    int a=count/2;
+    a--;
+
+    while(a>=0) {
+        pos=pos->next;
+        a--;       
+    }
+    return pos;
+    
+}
