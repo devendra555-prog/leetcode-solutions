73b6cdc9d70fcf9a4c1a186270d051b8ec50ab23
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-07-13 16:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (39.35%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,18 +1,4 @@
-// class Solution {
-// public:
-//     int sum(vector<int>& piles, int s, int l) {
-//         return piles[s] + piles[l];
-//     }
 
-//     bool stoneGame(vector<int>& piles) {
-//         int s = 0;
-//         int l = piles.size() - 1;
-
-//         int ans = sum(piles, s, l);
-
-//         return ans > 0;
-//     }
-// };
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
