74eaebc0edc5c626d5db4bcad0ebbbce781018e2
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-08-14 19:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (2.14%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,21 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int high =numsSize-1;
+    int low=0;
+    int mid;
+    while(low<=high) {
+        mid=(low+high)/2;
+        if(target==nums[mid]) {
+            return mid;
+        }
+        else if(target<nums[mid]) {
+            high=mid-1;
+        }
+        else {
+            low=mid+1;
+        }
+
+    }
+    return low;
+        
+    
+}
