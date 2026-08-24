aa58f5c9bb56399e7a4720e347ba3f42282ebc11
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-08-24 18:03:31 +0530

    Time: 3 ms (55.21%) | Memory: 9.2 MB (85.66%) - LeetSync

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,26 +1,18 @@
-#include <limits.h>
 
 int reverse(int x) {
     int temp = x;
-    int ans = 0;
+    long long ans = 0;
 
     while (temp != 0) {
         int rem = temp % 10;
         temp = temp / 10;
 
-
-        if (ans > INT_MAX / 10 || 
-            (ans == INT_MAX / 10 && rem > 7)) {
-            return 0;
-        }
-
-        if (ans < INT_MIN / 10 || 
-            (ans == INT_MIN / 10 && rem < -8)) {
-            return 0;
-        }
-
         ans = ans * 10 + rem;
     }
 
-    return ans;
+    if (ans > 2147483647 || ans < -2147483648LL) {
+        return 0;
+    }
+
+    return (int)ans;
 }
