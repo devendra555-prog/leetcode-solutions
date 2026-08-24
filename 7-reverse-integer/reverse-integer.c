a9f78482a94d045f70d0b51be0cf078e6e010b96
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-08-24 16:53:30 +0530

    Time: 7 ms (9.05%) | Memory: 9.2 MB (85.66%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,26 @@
+#include <limits.h>
+
+int reverse(int x) {
+    int temp = x;
+    int ans = 0;
+
+    while (temp != 0) {
+        int rem = temp % 10;
+        temp = temp / 10;
+
+        // Overflow check
+        if (ans > INT_MAX / 10 || 
+            (ans == INT_MAX / 10 && rem > 7)) {
+            return 0;
+        }
+
+        if (ans < INT_MIN / 10 || 
+            (ans == INT_MIN / 10 && rem < -8)) {
+            return 0;
+        }
+
+        ans = ans * 10 + rem;
+    }
+
+    return ans;
+}
