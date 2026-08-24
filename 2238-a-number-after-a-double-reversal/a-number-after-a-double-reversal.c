06823cdade8386dabbd3d708c83e4b9d10ebcd5e
Devendra Rathaur <devendrarathaur9555648026@gmail.com>
2026-08-24 21:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.00%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.c
@@ -0,0 +1,27 @@
+bool isSameAfterReversals(int num) {
+    int temp=num;
+    int ans=0;
+    int rem=0;
+    while(temp!=0) {
+        rem=temp%10;
+        temp=temp/10;
+        ans=ans*10 + rem ;
+
+    }
+    int temp1=ans;
+    int res=0;
+    rem=0;
+    while(temp1!=0) {
+        rem=temp1%10;
+        temp1=temp1/10;
+        res=res*10 +rem;
+    }
+    if(num==res) {
+        return true;
+    }
+    else    {
+        return false;
+    }
+
+    
+}
